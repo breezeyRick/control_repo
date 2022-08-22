ecc76fa63519b4245a73a19cdf8eac49417e4b12
BriToRick <105242643+breezeyRick@users.noreply.github.com>
2022-08-22 17:45:22 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,7 +7,7 @@ class minecraft{
     source => 'https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar',
   }
   package {'java':
-    ensure => present,
+    ensure => latest,
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
